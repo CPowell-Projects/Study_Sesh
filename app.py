e8b0977baa57d25070f3357fb55012927f9ca286
Atumi <Tehutihetep2@gmail.com>
2026-01-10 23:24:00 -0500

    Sort Assignments by due date

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
-from flask import Flask
-
+from flask import Flask, render_template, request
+from datetime import date
 app = Flask(__name__)
 
-@app.route("/")
+assignments = []
+
+@app.route("/", methods=["GET","POST"])
 def home():
-    return "Study-Sesh is running!"
+    error = None
+    if request.method == "POST":
+        if "delete_index" in request.form:
+            index = int(request.form["delete_index"])
+            if 0 <= index < len(assignments):
+                assignments.pop(index)
+
+        else:
+            course = request.form["course"]
+            due = request.form["due"]
+            today = date.today()
+            due_date =date.fromisoformat(due)
+            if due_date < today:
+                error = "Cannot input date earlier than today"
+            else: assignments.append({
+                 "course": course,
+                 "due": due
+
+        })
+            assignments.sort(
+                
+            key = lambda assignments: date.fromisoformat(assignments["due"])
+    )
+    return render_template("assignments.html", assignments = assignments, error=error)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
+ 
