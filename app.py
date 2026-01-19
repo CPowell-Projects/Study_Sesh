79c7b2c171f5121326c3688db02aac7e3e8ea80c
Atumi <Tehutihetep2@gmail.com>
2026-01-18 21:45:13 -0500

    Bug Fixes

--- app.py
+++ app.py
@@ -5,14 +5,15 @@ import os
 DATA_FILE = "assignments.json"
 app = Flask(__name__)
 
+
 if os.path.exists(DATA_FILE):
     try:
-        with open (DATA_FILE, "r" ) as f:
+        with open(DATA_FILE, "r" ) as f:
             assignments = json.load(f)
     except json.JSONDecodeError:
         assignments = []
-    else:
-        assignments =[]
+else:
+    assignments =[]
 def save_assignments():
     with open(DATA_FILE, "w") as f:
         json.dump(assignments, f, indent=2)
@@ -20,7 +21,8 @@ def save_assignments():
 @app.route("/", methods=["GET","POST"])
 def home():
     error = None
-    acton = None
+    action = None
+
     if request.method == "POST":
         action = request.form.get("action")
         if action == "delete":
@@ -28,28 +30,38 @@ def home():
             if 0 <= index < len(assignments):
                 assignments.pop(index)
                 save_assignments()
-                return redirect(url_for("home"))
+            return redirect(url_for("home"))
         
         elif action == "add":
-            course = request.form["course"]
-            due = request.form["due"]
-            today = date.today()
-            due_date =date.fromisoformat(due)
-            if due_date < today:
-                error = "Cannot input date earlier than today"
-            else: assignments.append({
-                 "course": course,
-                 "due": due
+            course = request.form.get("course")
+            due = request.form.get("due")
 
+            try:
+                due_date = date.fromisoformat(due)      
+                if due_date < date.today():
+                    error = "Cannot input date earlier than today"
+                else: assignments.append({
+                        "course": course,
+                        "due": due
         })
-        save_assignments()
+            except ValueError:
+                error = "Invalid date format"
+                               
         if error is None:
-            return redirect(url_for("home"))
-        assignments.sort(
+            
+        
+            assignments.sort(
+                key=lambda assignment: date.fromisoformat(assignment["due"])
                 
-            key=lambda assignment: date.fromisoformat(assignments["due"])
+            
     )
-    return render_template("assignments.html", assignments = assignments, error=error)
+            save_assignments()
+            return redirect(url_for("home"))
+
+    assignments.sort(
+        key=lambda a: date.fromisoformat(a["due"])
+    )
+    return render_template("assignments.html", assignments = assignments, error=error) 
 
 if __name__ == "__main__":
     app.run(debug=True)
